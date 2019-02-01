7227db4d1f245044b0d27fed6877465ba4bb2a36
admin <admin@nucciojohn.attlocal.net>
2019-02-01 11:58:09 -0600

    Forgot to update some changes last commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 let clicks = 0;
 
-let elem = document.getElementById('cat-to-click');
+let elem = document.getElementById('cat-to-click-one');
 elem.addEventListener('click', function(){
+    clicks++;
+    document.getElementById('number-of-clicks').innerHTML = clicks;
+  //the element has been clicked... do stuff here
+  console.log("Cat has been clicked " + clicks + " times!");
+}, false);
+
+let elem2 = document.getElementById('cat-to-click-two');
+elem2.addEventListener('click', function(){
     clicks++;
     document.getElementById('number-of-clicks').innerHTML = clicks;
   //the element has been clicked... do stuff here
